9e0969c47650f03c808a96af571a7d1fbf546b17
OutisLi <LTC201806070316@gmail.com>
2024-04-02 15:23:02 +0800

    finish ex1

---
+++ 1/ex1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+	puts("Hello word!");
+	printf("argc is %d\n",argc);
+	printf("argv[0] is %s\n",argv[0]);
+	printf("argv[1] is %s\n",argv[1]);
+	printf("argv[2] is %s\n",argv[2]);
+	return 0;
+}
