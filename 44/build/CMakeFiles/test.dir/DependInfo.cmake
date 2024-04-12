7f8419ea2aca6af8ff99f4e103ff6f827d484709
OutisLi <LTC201806070316@gmail.com>
2024-04-12 17:49:58 +0800

    finish ex44

---
+++ 44/build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/outisli/Documents/LCTHW/44/RingBuffer.c" "CMakeFiles/test.dir/RingBuffer.c.o" "gcc" "CMakeFiles/test.dir/RingBuffer.c.o.d"
+  "/home/outisli/Documents/LCTHW/44/bstrlib.c" "CMakeFiles/test.dir/bstrlib.c.o" "gcc" "CMakeFiles/test.dir/bstrlib.c.o.d"
+  "/home/outisli/Documents/LCTHW/44/test.c" "CMakeFiles/test.dir/test.c.o" "gcc" "CMakeFiles/test.dir/test.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 44/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/RingBuffer.c.o"
+  "CMakeFiles/test.dir/RingBuffer.c.o.d"
+  "CMakeFiles/test.dir/bstrlib.c.o"
+  "CMakeFiles/test.dir/bstrlib.c.o.d"
+  "CMakeFiles/test.dir/test.c.o"
+  "CMakeFiles/test.dir/test.c.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
